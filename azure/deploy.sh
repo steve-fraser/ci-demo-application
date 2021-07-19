53c36bf443a7469de5afc77df951944127f3f19f
Fraser, Steven (CarnCorp) <Sfraser@carnival.com>
2021-07-19 13:27:31 -0400

    images for the writeup

--- azure/deploy.sh
+++ azure/deploy.sh
@@ -19,19 +19,6 @@ az aks get-credentials --admin --name CircleCI-Cluster  --resource-group CircleC
 az ad sp create-for-rbac --name circleci-deploy-account 
 
 
-(⎈ |CircleCI-Cluster-admin:default)➜  .circleci git:(main) ✗ az ad sp create-for-rbac --name circleci-deploy-account
-In a future release, this command will NOT create a 'Contributor' role assignment by default. If needed, use the --role argument to explicitly create a role assignment.
-Creating 'Contributor' role assignment under scope '/subscriptions/[REDACTED]'
-The output includes credentials that you must protect. Be sure that you do not include these credentials in your code or check the credentials into your source control. For more information, see https://aka.ms/azadsp-cli
-'name' property in the output is deprecated and will be removed in the future. Use 'appId' instead.
-{
-  "appId": "[REDACTED]",
-  "displayName": "circleci-deploy-account",
-  "name": "[REDACTED]",
-  "password": "[REDACTED]",
-  "tenant": "[REDACTED]"
-}
-
 (⎈ |CircleCI-Cluster-admin:default)➜  .circleci git:(main) ✗ kubectl get pods
 NAME                                   READY   STATUS    RESTARTS   AGE
 ci-demo-application-5467fcb87d-z7dmn   1/1     Running   0          36s
